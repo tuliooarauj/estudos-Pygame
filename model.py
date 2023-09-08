b2cfc1b07cc2fb60655b171111fcecabd90cee98
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-09-08 09:46:41 -0300

    Update model.py

--- model.py
+++ model.py
@@ -6,22 +6,41 @@ pygame.init()
 
 largura = 640
 altura = 480
-x = largura
-y = 0
+x = largura / 2
+y = altura / 2
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
+relogio = pygame.time.Clock()
 
 while True:
+    relogio.tick(30)
     tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+            
+        '''if event.type == KEYDOWN:
+            if event.key == K_a:
+                x -= 20
+            if event.key == K_d:
+                x += 20
+            if event.key == K_w:
+                y -= 20
+            if event.key == K_s:
+                y += 20'''
+        
+        if pygame.key.get_pressed()[K_a]:
+            x -= 20
+        if pygame.key.get_pressed()[K_d]:
+            x += 20
+        if pygame.key.get_pressed()[K_w]:
+            y -= 20
+        if pygame.key.get_pressed()[K_s]:
+            y += 20
+
     pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50)) #pos(200, 300), width = 40px, height =  50px
-    if y >= altura:
-        y = 0
-    y += 1
     #pygame.draw.circle(tela, (0, 0, 255), (300, 260), 40)
     #pygame.draw.line(tela, (255, 255, 0), (390, 0), (390,  600), 5)
 
