2b0713a271009c936b5613d70c1c17e917d3e0f4
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-09-08 10:30:25 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -9,27 +10,20 @@ altura = 480
 x = largura / 2
 y = altura / 2
 
+x_azul = randint(40, 600)
+y_azul = randint(50, 430)
+
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(30)
+    relogio.tick(60)
     tela.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-            
-        '''if event.type == KEYDOWN:
-            if event.key == K_a:
-                x -= 20
-            if event.key == K_d:
-                x += 20
-            if event.key == K_w:
-                y -= 20
-            if event.key == K_s:
-                y += 20'''
         
         if pygame.key.get_pressed()[K_a]:
             x -= 20
@@ -40,8 +34,11 @@ while True:
         if pygame.key.get_pressed()[K_s]:
             y += 20
 
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50)) #pos(200, 300), width = 40px, height =  50px
-    #pygame.draw.circle(tela, (0, 0, 255), (300, 260), 40)
-    #pygame.draw.line(tela, (255, 255, 0), (390, 0), (390,  600), 5)
+    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50)) #pos(200, 300), width = 40px, height =  50px
+    ret_azul = pygame.draw.rect(tela, (0, 255, 0), (x_azul, y_azul, 40, 50))
+
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40, 600)
+        y_azul = randint(50, 430)
 
     pygame.display.update()
